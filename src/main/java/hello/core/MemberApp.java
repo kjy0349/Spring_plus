c2c3cf4b1850103304fe04fe8047ef0b0ee6e2b7
kjy0349 <kjy0349@naver.com>
2022-06-06 20:34:53 +0900

    order

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,13 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
+
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
